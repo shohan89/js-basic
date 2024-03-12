a127282bbc75e863af9ea7fa7e86bb386e0d3c87
Shohan Sharoar <maffin.killer007@gmail.com>
2024-03-12 22:10:29 +0600

    Comparison operator added.

---
+++ comparison.js
@@ -0,0 +1,13 @@
+/**
+ * COMPARISON
+ * 1. Greater  than (>)
+ * 2. Less than (<)
+ * 3. Equal to (=)
+ * 4. Greater than or  equal to (>=)
+ * 5. Less than or equal to (<=)
+ * 6. Not  equal (!=)
+ * 
+ * ====== Special or Double condition =
+ * 7. And  operator (&&): It returns true only if both the conditions are true.
+ * 8.  Or operator (||): It returns true if either of the conditions is true.
+ */

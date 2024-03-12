03211801ac87cf50d8fe100379bc1bc4ad399973
Shohan Sharoar <maffin.killer007@gmail.com>
2024-03-13 01:35:50 +0600

    If else added

---
+++ condition.js
@@ -0,0 +1,49 @@
+// Condition 
+/**
+ * If you buy food for me 
+ * I will come
+ * 
+ * -------------------------
+ * if biriyani price less than 300tk
+ *    I will buy
+ * 
+ * 
+ * ------- STRUCTURE --------
+ * if (condition){
+ *      code will execute if condition above is true... 
+ * }
+ * 
+ */
+
+// Example:
+if( 3 < 10 ){
+    console.log('I have a smaller value!')
+} 
+
+/**
+ * else statement
+ * -----------------------
+ * 
+ * if the first condition is false, then it checks this one..
+ * 
+ * -------- STRUCTURE --------
+ * 
+ * if (condition){
+ *      execute code if condition
+ *      above is true;
+ * }
+ * else{
+ *    execute code if condition
+ *      above is false;
+ * }
+ */
+
+// Example:
+var weight = 40;
+
+if (weight < 20) {
+    console.log('I will carry it by myself');
+}
+else {
+    console.log('I need to ask for help');
+}

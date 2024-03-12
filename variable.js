7f354ddf9795022320f0de96da0e189cc3fcc48d
Shohan Sharoar <maffin.killer007@gmail.com>
2024-03-12 22:04:17 +0600

    All Variable added

---
+++ variable.js
@@ -0,0 +1,29 @@
+// Variable types
+
+// Number
+var sunglassPrice = 80;
+
+// Boolean
+var isPassed = true;
+
+// String
+var name = "John";
+
+/***
+ * 1. Single Word
+ * 2. No quote
+ * 3. No gap or dash
+ * 4. Can not start with number
+ * 5. Can not be a keyword  (e.g., class, let)
+ * 6. Prefer camelCase
+ * 
+ */
+
+/**
+ * Operation:
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=, %=
+ * 3. isNaN
+ * 4. null
+ * 5. Number
+ */

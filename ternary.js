3c14b03bb9f1584aec7a456ed0e2a554d4f87847
shoh4n <maffin.killer007@gmail.com>
2024-03-13 12:38:22 +0600

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,14 @@
+/**
+ * TERNARY --> Three parts
+ * 
+ *  ?    :
+ * 
+ *  condition ? do something when true : do something when false
+ * 
+ */
+
+// Example:
+
+var age =  18;
+
+age >= 18 ? console.log('Vote dio') : console.log('Ghumai thako');

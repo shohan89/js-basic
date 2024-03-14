4d7483c1b46f9cd410c0cacf0e5ae4824257bd81
shoh4n <maffin.killer007@gmail.com>
2024-03-14 15:37:08 +0600

    Array Length, Index, Get And Set By Index

--- array.js
+++ array.js
@@ -4,3 +4,28 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 const friends = ['Abul', 'Babul', 'Cabul', 'Dabul'];
 const passed = [ true, false, false, true, false ];
 const mixedArray = [ 12, 'Abdul', 34.87, true ];
+
+
+// ========================================
+
+// Check Array length by using 'length'
+console.log(mixedArray.length);
+
+// Array Item position count starts from 0 and ends from length - 1.
+
+/**
+ * 
+ *  0 => 1 => 2 => 3 => 4 and so on...
+ */
+
+// Example: 
+console.log(numbers[3]); // get element value by index
+
+/**
+ * We can set any element of the array to another variable
+ */
+// Example:
+
+const fourth = numbers[3];
+
+// set or update element value by index

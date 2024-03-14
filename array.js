ebe2e5bd48a65bf4ae3e596bcb810a3a6ddf719c
shoh4n <maffin.killer007@gmail.com>
2024-03-14 15:20:07 +0600

    Array

---
+++ array.js
@@ -0,0 +1,6 @@
+// array
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const friends = ['Abul', 'Babul', 'Cabul', 'Dabul'];
+const passed = [ true, false, false, true, false ];
+const mixedArray = [ 12, 'Abdul', 34.87, true ];

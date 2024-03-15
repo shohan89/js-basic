9334ad5897f8dd29a24373025a5b26dae6a088d8
Shohan Sharoar <maffin.killer007@gmail.com>
2024-03-16 01:09:16 +0600

    for of loop

---
+++ for-of.js
@@ -0,0 +1,9 @@
+const numbers = [10, 54, 56, 87, 66, 88, 77 ];
+
+for ( const num of numbers ){
+    if( numbers == [] ){
+        console.log("Loop finished!");
+    }else{
+        console.log('The number is: ' + num);
+    }
+}
